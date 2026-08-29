7dc8cd4257a1ceca9ea61776761c863de54a1edb
cnaresr <cezar.nares@gmail.com>
2026-08-29 20:49:48 +0700

    Fix: Admin CMS favicon uniformization

--- resources/views/layouts/cms.blade.php
+++ resources/views/layouts/cms.blade.php
@@ -6,6 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title', 'CMS Portal - University News')</title>
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><rect width='100' height='100' fill='%23B71032'/><text x='50' y='50' font-family='sans-serif' font-weight='bold' font-size='70' fill='white' dominant-baseline='central' text-anchor='middle'>U</text></svg>">
 
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
